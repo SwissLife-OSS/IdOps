c6d1ac8a3c07ac9e9f7f2641b4401a23c7d11d31
glucaci <gabriel.lucaci.dev@gmail.com>
2021-11-11 13:33:31 +0100

    Cleanup

--- src/Server/src/Core/Data/Templates/ClientTemplateService.cs
+++ src/Server/src/Core/Data/Templates/ClientTemplateService.cs
@@ -56,12 +56,9 @@ namespace IdOps.Templates
             client.ClientId = GetClientId(template);
             client.Name = GetName(template, application, environment);
             client.ClientUri = GetUri(template, application, environment)?.ToLower();
-            client.RedirectUris = template.RedirectUris.Select(t => RenderTemplate(t, application, environment, client))
-                .Union(application.RedirectUris.Select(t => RenderTemplate(t, application, environment, client)))
-                .ToList();
-            client.AllowedGrantTypes = template.AllowedGrantTypes
-                .Union(application.AllowedGrantTypes)
-                .ToList();
+            client.RedirectUris = template.RedirectUris.Union(application.RedirectUris)
+                .Select(uri => RenderTemplate(uri, application, environment, client)).ToList();
+            client.AllowedGrantTypes = template.AllowedGrantTypes.Union(application.AllowedGrantTypes).ToList();
             client.AllowedScopes = BuildScopes(application, client);
             client.AllowAccessTokensViaBrowser = template.AllowAccessTokensViaBrowser;
             client.AllowOfflineAccess = template.AllowOfflineAccess;
@@ -96,16 +93,12 @@ namespace IdOps.Templates
             Model.Environment environment = await _environmentService
                 .GetByIdAsync(client.Environments.Single(), cancellationToken);
 
-            client.AllowedGrantTypes = client.AllowedGrantTypes
-                .Union(application.AllowedGrantTypes)
-                .ToList();
+            client.AllowedGrantTypes = client.AllowedGrantTypes.Union(application.AllowedGrantTypes).ToList();
             client.AllowedScopes = BuildScopes(application, client);
             client.AllowAccessTokensViaBrowser = template.AllowAccessTokensViaBrowser != client.AllowAccessTokensViaBrowser
-                ? client.AllowAccessTokensViaBrowser
-                : template.AllowAccessTokensViaBrowser;
-            client.RedirectUris = client.RedirectUris
-                .Union(application.RedirectUris.Select(t => RenderTemplate(t, application, environment, client)))
-                .ToList();
+                ? client.AllowAccessTokensViaBrowser : template.AllowAccessTokensViaBrowser;
+            client.RedirectUris = client.RedirectUris.Union(application.RedirectUris)
+                .Select(uri => RenderTemplate(uri, application, environment, client)).ToList();
 
             return client;
         }
