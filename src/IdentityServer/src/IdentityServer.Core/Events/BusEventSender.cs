9b0aa3fa4e1e28645e1b9af074184638ac1a5880
Gabriel Lucaci <gabriel.lucaci.dev@gmail.com>
2022-02-09 15:59:27 +0100

    Refactor event sinks (#41)

---
+++ src/IdentityServer/src/IdentityServer.Core/Events/BusEventSender.cs
@@ -0,0 +1,42 @@
+using System.Threading;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using IdOps.Messages;
+using MassTransit;
+using Microsoft.Extensions.Hosting;
+
+namespace IdOps.IdentityServer.Events
+{
+    public class BusEventSender : BackgroundService
+    {
+        private readonly IBus _bus;
+        private readonly ChannelReader<IdentityEventMessage> _channelReader;
+
+        public BusEventSender(
+            IBus bus,
+            ChannelReader<IdentityEventMessage> channelReader)
+        {
+            _bus = bus;
+            _channelReader = channelReader;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            await Task.Yield();
+
+            try
+            {
+                while (await _channelReader.WaitToReadAsync(stoppingToken))
+                {
+                    IdentityEventMessage entity = await _channelReader.ReadAsync(stoppingToken);
+
+                    await _bus.Publish(entity, stoppingToken);
+                }
+            }
+            catch
+            {
+                // ignored
+            }
+        }
+    }
+}
--- src/IdentityServer/src/IdentityServer.Core/Events/IdOpsEventSink.cs
+++ src/IdentityServer/src/IdentityServer.Core/Events/IdOpsEventSink.cs
@@ -1,9 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using Duende.IdentityServer.Events;
 using Duende.IdentityServer.Services;
-using IdOps.IdentityServer.Extensions;
 using Microsoft.Extensions.Logging;
 
 namespace IdOps.IdentityServer.Events
@@ -21,32 +21,20 @@ namespace IdOps.IdentityServer.Events
             _sinks = sinks;
         }
 
-        public Task PersistAsync(Event evt)
+        public async Task PersistAsync(Event evt)
         {
             try
             {
-                Task.Run(() => ProcessAsync(evt)).Forget();
+                Activity? activity = Activity.Current;
+
+                foreach (IIdOpsEventSink sink in _sinks)
+                {
+                    await sink.ProcessAsync(evt, activity);
+                }
             }
             catch (Exception ex)
             {
                 _logger.LogError(ex, "Error while sending event");
-            }
-
-            return Task.CompletedTask;
-        }
-
-        private async Task ProcessAsync(Event evt)
-        {
-            foreach (IIdOpsEventSink sink in _sinks)
-            {
-                try
-                {
-                    await sink.ProcessAsync(evt);
-                }
-                catch (Exception ex)
-                {
-                    _logger.LogError(ex, $"Error while processing event {sink.GetType().FullName}");
-                }
             }
         }
     }
