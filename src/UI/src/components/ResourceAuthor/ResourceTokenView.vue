9f48d6b6f72766d7aa101a4d344d8a6e3cbdb9e7
Fynn Brock <fynn.brock@swisslife.ch>
2023-05-17 12:44:47 +0200

    Add table with available flows

--- src/UI/src/components/ResourceAuthor/ResourceTokenView.vue
+++ src/UI/src/components/ResourceAuthor/ResourceTokenView.vue
@@ -1,9 +1,35 @@
 <template>
-  <div>
-    <v-switch label="Enable Token generation" v-model="allowTokenGeneration" :disabled="!clientHasSavedSecrets"></v-switch>
-    <v-switch label="Log allowTokenGeneration" @click="log" ></v-switch>
-  </div>
-  
+  <v-card flat>
+    <div>
+      <v-switch label="Enable Token generation" v-model="allowTokenGeneration" :disabled="!hasSavedSecrets"></v-switch>
+      <v-switch label="Log allowTokenGeneration" @click="log({ allowTokenGeneration })"></v-switch>
+    </div>
+    <v-toolbar
+     elevation="0"
+      color="grey lighten-6"
+      height="38">
+      <v-toolbar-title>Available Tokenflows</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-icon @click="log({ grantTypes })">mdi-refresh</v-icon>
+    </v-toolbar>
+    <v-card-text>
+      <v-data-table
+      height="300"
+      :headers="headers"
+      :items="listItems"
+      item-key="id"
+      hide-default-footer>
+        <template v-slot:item.id="{ item }">
+          <td>{{ item.id }}</td>
+        </template>
+        <template v-slot:item.action="{ item }">
+          <v-btn :disabled="!allowTokenGeneration">
+            {{ item.action }}
+          </v-btn>
+        </template>
+      </v-data-table>
+    </v-card-text>
+  </v-card>
 </template>
 
 
@@ -19,16 +45,45 @@ export default {
     return {
       allowTokenGeneration: false,
       client: {},
+      headers: [
+        {
+          text: "Grant Type",
+          width: 100,
+          align: "center",
+          value: "id",
+          sortable: false
+        },
+        {
+          text: "Generate",
+          align: "center",
+          value: "action",
+          sortable: false
+        }
+      ]
     }
   },
   computed: {
-    secrets: function() {
+    secrets: function () {
       return this.client.clientSecrets
     },
-    clientHasSavedSecrets: function() {
+    hasSavedSecrets: function () {
       //undefined if client hasn't been set because of async
-      if (this.secrets === undefined) return false;
-      return this.secrets.some(secret => secret.encryptedSecret !== null);
+      if (!this.secrets) return false;
+      return this.secrets.some(secret => secret.encryptedSecret !== null)
+    },
+    grantTypes: function () {
+      //undefined if client hasn't been set because of async
+      if (!this.client.allowedGrantTypes) return [];
+      return this.client.allowedGrantTypes
+    },
+    listItems: function () {
+      console.log('grantTypes:', this.grantTypes);
+      const items = this.grantTypes.map(grantType => ({
+        id: grantType,
+        action: "Generate " + grantType + " Token"
+      }));
+      console.log('listItems:', items);
+      return items;
     }
   },
   methods: {
@@ -41,8 +96,8 @@ export default {
 
       delete this.client.__typename;
     },
-    log() {
-      console.log(this.allowTokenGeneration)
+    log(message) {
+      console.log(message)
     }
 
   },
