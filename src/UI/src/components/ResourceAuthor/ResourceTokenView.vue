eac8fa7044636cc33762365b2b7b93a052a795a7
Fynn Brock <fynn.brock@swisslife.ch>
2023-05-23 11:25:21 +0200

    Add dummy token dialog

--- src/UI/src/components/ResourceAuthor/ResourceTokenView.vue
+++ src/UI/src/components/ResourceAuthor/ResourceTokenView.vue
@@ -1,37 +1,38 @@
 <template>
-  <v-card flat>
-    <div>
-      <v-switch label="Enable Token generation" v-model="allowTokenGeneration" :disabled="!hasSavedSecrets"></v-switch>
-      <v-switch label="Log allowTokenGeneration" @click="log({ allowTokenGeneration })"></v-switch>
-    </div>
-    <v-toolbar
-    elevation="0"
-    color="grey lighten-6"
-    height="38">
-      <v-toolbar-title>Available Tokenflows</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-icon @click="log({ grantTypes })">mdi-refresh</v-icon>
-    </v-toolbar>
-    <v-card-text>
-      <v-data-table
-      height="300"
-      :headers="headers"
-      :items="listItems"
-      item-key="id"
-      hide-default-footer>
-        <template v-slot:item.id="{ item }">
-          <td>{{ item.id }}</td>
-        </template>
-        <template v-slot:item.action="{ item }">
-          <v-btn
-          @click="startTokenFlow(item.id)"
-          :disabled="!allowTokenGeneration">
-            {{ item.action }}
-          </v-btn>
-        </template>
-      </v-data-table>
-    </v-card-text>
-  </v-card>
+  <div>
+    <v-card flat>
+      <div>
+        <v-switch label="Enable Token generation" v-model="allowTokenGeneration" :disabled="!hasSavedSecrets"></v-switch>
+        <v-switch label="Log allowTokenGeneration" @click="log({ allowTokenGeneration })"></v-switch>
+        <v-switch label="Log openTokenAlert" @click="log({ openTokenAlert })"></v-switch>
+        <v-switch label="Log currentGrantType" @click="log({ currentGrantType })"></v-switch>
+      </div>
+      <v-toolbar elevation="0" color="grey lighten-6" height="38">
+        <v-toolbar-title>Available Tokenflows</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-icon @click="log({ grantTypes })">mdi-refresh</v-icon>
+      </v-toolbar>
+      <v-card-text>
+        <v-data-table height="300" :headers="headers" :items="listItems" item-key="id" hide-default-footer>
+          <template v-slot:item.id="{ item }">
+            <td>{{ item.id }}</td>
+          </template>
+          <template v-slot:item.action="{ item }">
+            <v-btn @click="startTokenFlow(item.id)" :disabled="!allowTokenGeneration">
+              {{ item.action }}
+            </v-btn>
+          </template>
+        </v-data-table>
+      </v-card-text>
+    </v-card>
+      <ClientCredentialsTokenAlert
+     :id = "id"
+     :activator.sync= "openTokenAlert"
+     :grantType.sync="currentGrantType"
+     >
+    </ClientCredentialsTokenAlert>
+  </div>
+
 </template>
 
 
@@ -41,77 +42,85 @@
 <script>
 import { getClientById } from "../../services/idResourceService";
 import { startTokenFlow } from "../../services/tokenFlowService";
+import ClientCredentialsTokenAlert from "./TokenFlows/ClientCredentialsTokenAlert";
 
 export default {
-  props: ["id"],
-  data() {
-    return {
-      allowTokenGeneration: false,
-      client: {},
-      headers: [
-        {
-          text: "Grant Type",
-          width: 100,
-          align: "center",
-          value: "id",
-          sortable: false
+    props: ["id"],
+    components:{
+      ClientCredentialsTokenAlert
+    },
+    data() {
+        return {
+            allowTokenGeneration: false,
+            openTokenAlert: false,
+            currentGrantType: "none",
+            client: {},
+            headers: [
+                {
+                    text: "Grant Type",
+                    width: 100,
+                    align: "center",
+                    value: "id",
+                    sortable: false
+                },
+                {
+                    text: "Generate",
+                    align: "center",
+                    value: "action",
+                    sortable: false
+                }
+            ]
+        };
+    },
+    computed: {
+        secrets: function () {
+            return this.client.clientSecrets;
         },
-        {
-          text: "Generate",
-          align: "center",
-          value: "action",
-          sortable: false
+        hasSavedSecrets: function () {
+            //undefined if client hasn't been set because of async
+            if (!this.secrets)
+                return false;
+            return this.secrets.some(secret => secret.encryptedSecret !== null);
+        },
+        grantTypes: function () {
+            //undefined if client hasn't been set because of async
+            if (!this.client.allowedGrantTypes)
+                return [];
+            return this.client.allowedGrantTypes;
+        },
+        listItems: function () {
+            return this.grantTypes.map(grantType => ({
+                id: grantType,
+                action: "Generate " + grantType + " Token"
+            }));
         }
-      ]
-    }
-  },
-  computed: {
-    secrets: function () {
-      return this.client.clientSecrets;
     },
-    hasSavedSecrets: function () {
-      //undefined if client hasn't been set because of async
-      if (!this.secrets) return false;
-      return this.secrets.some(secret => secret.encryptedSecret !== null);
+    methods: {
+        async setClient() {
+            this.loading = true;
+            const result = await getClientById(this.id);
+            this.loading = false;
+            const { client } = result.data;
+            this.client = Object.assign({}, client);
+            delete this.client.__typename;
+        },
+        async startTokenFlow(grantType) {
+            await startTokenFlow(grantType);
+            this.currentGrantType = grantType;
+            this.openTokenAlert = true;
+        },
+        log(message) {
+            console.log(message);
+        }
     },
-    grantTypes: function () {
-      //undefined if client hasn't been set because of async
-      if (!this.client.allowedGrantTypes) return [];
-      return this.client.allowedGrantTypes;
+    watch: {
+        id: {
+            immediate: true,
+            handler: function () {
+                this.setClient();
+            },
+        }
     },
-    listItems: function () {
-      return this.grantTypes.map(grantType => ({
-        id: grantType,
-        action: "Generate " + grantType + " Token"
-      }));
-    }
-  },
-  methods: {
-    async setClient() {
-      this.loading = true;
-      const result = await getClientById(this.id);
-      this.loading = false;
-      const { client } = result.data;
-      this.client = Object.assign({}, client);
-
-      delete this.client.__typename;
-    },
-    async startTokenFlow(grantType) {
-      await startTokenFlow(grantType)
-    },
-    log(message) {
-      console.log(message);
-    }
-  },
-  watch: {
-    id: {
-      immediate: true,
-      handler: function () {
-        this.setClient();
-      },
-    }
-  }
-
 };
 </script>
 
